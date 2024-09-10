9619a8ea1645554e5a63ff68d01b95da07623340
juleswg23 <jules.walzergoldfeld@gmail.com>
2024-09-10 12:07:52 -0400

    added borders

--- sketch.js
+++ sketch.js
@@ -4,13 +4,24 @@ const KeyS = 83;
 const KeyD = 68;
 
 const PAGE_SIZE = 1_000;
-const PLAYER_HEIGHT = 50;
+const PLAYER_HEIGHT = 20;
 const STEP_SIZE = 10;
-let sunHeight;
+
+const PLAYER1_COLOR = "navy";
+const PLAYER2_COLOR = "red";
+const BORDER_COLOR = "black";
+const BORDER_WIDTH = PLAYER_HEIGHT/10;
+const BORDER_FREQ = 0.6;
+
+const GRID_ORDER = 20;
 
 let player1;
 let player2;
 let players;
+
+let bg;
+let grid;
+let borders = [];
 
 class Player{
   constructor(x, y, height, stepSize, playerBool) {
@@ -21,17 +32,13 @@ class Player{
     this.isPlayer1 = playerBool;
   }
 
-  move(params) {
-    
-  }
-
   show() {
     switch (this.isPlayer1) {
       case true:
-        fill("navy");
+        fill(PLAYER1_COLOR);
         break
       default:
-        fill("red");
+        fill(PLAYER2_COLOR);
     }
   
     circle(this.x, this.y-(this.height/4), this.height/2);
@@ -41,38 +48,61 @@ class Player{
 
 }
 
+class Border {
+  constructor(x, y, size, isHorizontal) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.isHorizontal = isHorizontal;
+  }
+
+  show(pg) {
+    fill(BORDER_COLOR);
+    strokeWeight(BORDER_WIDTH);
+
+    switch (this.isHorizontal) {
+      case true:
+        pg.line(this.x, this.y, this.x+this.size, this.y);
+        break;
+      default:
+        pg.line(this.x, this.y, this.x, this.y+this.size);
+    }
+    return pg;
+  }
+
+}
+
 function drawPlayers() { 
 
   // Steady movement, 1/3rd step
   if (keyIsDown(UP_ARROW)) {
-    player2.y-=STEP_SIZE/3;
+    player2.y-=STEP_SIZE/2;
   }
   if (keyIsDown(RIGHT_ARROW)) {
-    player2.x+=STEP_SIZE/3;
+    player2.x+=STEP_SIZE/2;
   }
   if (keyIsDown(LEFT_ARROW)) {
-    player2.x-=STEP_SIZE/3;
+    player2.x-=STEP_SIZE/2;
   }
   if (keyIsDown(DOWN_ARROW)) {
-    player2.y+=STEP_SIZE/3;
+    player2.y+=STEP_SIZE/2;
   }
   
   if (keyIsDown(KeyW)) {
-    player1.y-=STEP_SIZE/3;
+    player1.y-=STEP_SIZE/2;
   }
   if (keyIsDown(KeyA)) {
-    player1.x-=STEP_SIZE/3;
+    player1.x-=STEP_SIZE/2;
   }
   if (keyIsDown(KeyS)) {
-    player1.y+=STEP_SIZE/3;
+    player1.y+=STEP_SIZE/2;
   }
   if (keyIsDown(KeyD)) {
-    player1.x+=STEP_SIZE/3;
+    player1.x+=STEP_SIZE/2;
   }
 
   // Collision logic with wall
   for (let player of players) {
-    console.log(player);
     if (player.x+PLAYER_HEIGHT/5 > PAGE_SIZE) {
       player.x = PAGE_SIZE-PLAYER_HEIGHT/5;
     } else if (player.x-PLAYER_HEIGHT/5 < 0) {
@@ -89,26 +119,58 @@ function drawPlayers() {
   player2.show();
 }
 
+function generateBorders(background) {
+  // create horizontal borders
+  for (let i = 1; i < GRID_ORDER-1; i++) {
+    for (let j = 1; j < GRID_ORDER; j++) {
+      if (Math.random() < BORDER_FREQ) {
+        borders.push(new Border(i*PAGE_SIZE/GRID_ORDER, j*PAGE_SIZE/GRID_ORDER, PAGE_SIZE/GRID_ORDER, true));
+      }
+    }
+  }
+
+  // create vertical borders
+  for (let i = 1; i < GRID_ORDER; i++) {
+    for (let j = 1; j < GRID_ORDER-1; j++) {
+      if (Math.random() < BORDER_FREQ) {
+        borders.push(new Border(i*PAGE_SIZE/GRID_ORDER, j*PAGE_SIZE/GRID_ORDER, PAGE_SIZE/GRID_ORDER, false));
+      }
+    }
+  }
+
+  // add the borders to background
+  for (let border of borders) {
+    background = border.show(background);
+  }
+
+  return background;
+}
+
 function setup() {
   player1 = new Player(100, 100, PLAYER_HEIGHT, STEP_SIZE, true);
   player2 = new Player(PAGE_SIZE-100, PAGE_SIZE-100, PLAYER_HEIGHT, STEP_SIZE, false);
-
   players = [player1, player2];
 
   createCanvas(PAGE_SIZE, PAGE_SIZE);
+  // createBorderOutline()
+
+  bg = createGraphics(PAGE_SIZE, PAGE_SIZE);
+  bg.background(225);
+  bg = generateBorders(bg);
+
 }
 
 function draw() {
-  background(200);
+  clear();
+  image(bg, 0, 0);
 
-  drawPlayers();
+  strokeWeight(2); drawPlayers();
 }
-
 
 // Starting movement, full step
 function keyPressed() {
   if (key === 'w') {
-    player1 -= STEP_SIZE;
+    player1.y -= STEP_SIZE;
   } else if (key === 'a') {
     player1.x -= STEP_SIZE;
   } else if (key === 's') {
